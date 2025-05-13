8015906926e9cf06764d1db874f5836c82aa2fb0
Fabian Sasse <faba@Fabians-MacBook-Pro.local>
2025-05-13 22:36:09 +0200

    WIP: loading spinner on index

--- static/js/Loading.js
+++ static/js/Loading.js
@@ -8,3 +8,13 @@ function hideLoading() {
 
 // Optional: Hide it automatically on page load (if it's still visible for any reason)
 window.addEventListener('load', () => hideLoading());
+
+function showLoading() {
+    document.getElementById('loading-spinner').classList.add('show');
+}
+
+function hideLoading() {
+    document.getElementById('loading-spinner').classList.remove('show');
+}
+
+window.addEventListener('load', () => hideLoading());
