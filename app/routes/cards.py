5184fcb24647e2c70a2eafc3eaec6fee87c84c8f
Fabian Sasse <faba@Fabians-MacBook-Pro.local>
2025-05-15 08:54:36 +0200

    WIP: Infinte scroll on most pages

--- app/routes/cards.py
+++ app/routes/cards.py
@@ -12,13 +12,25 @@ card_bp = Blueprint("cards", __name__)
 def index():
     cards = []
     error = None
+    page = request.args.get("page", 1, type=int)
+    per_page = 20
 
+    query = None
     if request.method == "POST":
         query = request.form.get("query")
-        if query:
-            cards = fetch_and_cache_cards(search_string=query)
+    elif request.method == "GET":
+        query = request.args.get("query")
 
-    return render_template("index.html", cards=cards, error=error)
+    if query:
+        cards = fetch_and_cache_cards(search_string=query, page=page, per_page=per_page)
+
+    # AJAX: return only the cards grid partial
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        if not cards:
+            return '', 204
+        return render_template("partials/card_grid.html", cards=cards)
+
+    return render_template("index.html", cards=cards, error=error, query=query)
 
 
 @card_bp.route("/sets", methods=["GET"])
@@ -42,7 +54,7 @@ def sets():
     return render_template("sets.html", sets=sets)
 
 @card_bp.route('/sets/<set_code>')
-def show_set(set_code):
+def set_detail(set_code):
     page = request.args.get('page', 1, type=int)
     selected_set = Set.query.filter_by(code=set_code).first_or_404()
     cards = fetch_and_cache_cards(
