c392b0ccb998b54883a71533689ef6492ebeabcd
Fabian Sasse <fabaderheld@gmail.com>
2025-05-20 06:36:08 +0200

    WIP: Added images to landing page

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,14 @@
 #from ._cards import card_bp
 from ..mtg.routes import mtg_bp
 from ..lorcana.routes import lorcana_bp
+from flask import render_template
 
 def register_routes(app):
     #app.register_blueprint(card_bp)
     app.register_blueprint(mtg_bp, url_prefix='/mtg')
     app.register_blueprint(lorcana_bp, url_prefix='/lorcana')
+
+# Register the landing page route directly on the app
+    @app.route('/')
+    def landing():
+        return render_template('landing.html')
